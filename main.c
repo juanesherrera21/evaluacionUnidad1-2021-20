15023233cfc342fa35b1dcc6ed255c2fca92712f
juanesherrera21 <68380237+juanesherrera21@users.noreply.github.com>
2021-08-23 21:29:07 -0500

    Feature 1
    Ya en el archivo de salida se esta escribiendo la primera linea del archivo de lectura.

--- main.c
+++ main.c
@@ -3,6 +3,41 @@
 #include "student.h"
 
 int main(int argc, char *argv[]){
+// Lectura del archivo
+
+
+int c=0;
+char temporal[150];
+
+
+FILE *inFile=fopen("fin","r");
+if(inFile==NULL){
+    perror("Error en la apertura del archivo");
+    return (EXIT_FAILURE);
+}
+
+FILE *salida;
+
+if((salida=fopen("salida.txt","w"))==NULL){
+    
+    exit(1);
+}
+
+ 
+while(!feof(inFile)){
+fgets(temporal,150,inFile);
+
+if(c==0){
+    fputs(temporal,salida);
+}
+c++;
+}
+rewind(inFile);
+
+fclose(inFile);
+fclose(salida);
 
     return EXIT_SUCCESS;
+
+
 }
