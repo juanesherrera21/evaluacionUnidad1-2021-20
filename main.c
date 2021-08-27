4fbe42c4310e2d1fa973dfe5844c2c07380f1dc4
juanesherrera21 <68380237+juanesherrera21@users.noreply.github.com>
2021-08-27 12:04:22 -0500

    Correccion feature 1 y feature 2
    los feature anteriores se estaban trabajando y construllendo todo en el main.c, ahora se realizo un cambio y se estan trabajando en student.c el codigo de todos esots y en el main.c solo los estamos llamando

--- main.c
+++ main.c
@@ -1,91 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "student.h"
-#include <string.h>
-#include <ctype.h>
-
 
 int main(int argc, char *argv[]){
-// Lectura del archivo
-
-
-int c=0;
-char temporal[70];
-
-
-FILE *inFile=fopen("fin","r");
-if(inFile==NULL){
-    perror("Error en la apertura del archivo");
-    return (EXIT_FAILURE);
-}
-
-FILE *salida;
-
-if((salida=fopen("salida.txt","w"))==NULL){
-    
-    exit(1);
-}
-
- 
-while(!feof(inFile)){
-fgets(temporal,70,inFile);
-
-if(c==0){
-    fputs(temporal,salida);
-}
-if(c==1){
-int pi,pf;
-char aux;
-
-pi=0;
-pf=strlen(temporal)-1;
-while(pi<pf){
-aux=temporal[pi];
-temporal[pi]=temporal[pf];
-temporal[pf]=aux;
-pi++; 
-pf--;
-}
-fputs(temporal,salida);
-}
-if(c==2){
-fputs(temporal,salida);
-}
-if(c==3){
-char *token;
-int sumatotal;
-sumatotal=0;
-token = strtok(temporal, " ");
-if(token==NULL){
-printf("NO hay token");
-}
-sumatotal += atoi(token);
-while(token!= NULL){
-token=strtok(NULL, " ");
-if(token!=NULL){
-sumatotal+=atoi(token);
-}
-}
-fprintf(salida,"%d\n",sumatotal);
-
-}
-if(c==4){
-fputs(temporal,salida);
-}
-if(c==5){
-
-}
-if(c==6){
-
-}
-c++;
-}
-rewind(inFile);
-
-fclose(inFile);
-fclose(salida);
-
+	
+	feature1(argv[1],argv[2]);
+	
+	feature2(argv[1],argv[2]);
     return EXIT_SUCCESS;
-
-
 }
--- student.c
+++ student.c
@@ -1,3 +1,77 @@
 #include <stdlib.h>
 #include "student.h"
+//FEATURE1
+void feature1(FILE *fin, FILE *fout){
+int c=0;
+char temporal[70];
 
+
+fin=fopen("fin","r");
+if(fin==NULL){
+    perror("Error en la apertura del archivo");
+    return (EXIT_FAILURE);
+}
+
+
+if((fout=fopen(fout,"w"))==NULL){
+    
+    exit(1);
+}
+
+ 
+while(!feof(fin)){
+fgets(temporal,70,fin);
+
+if(c==0){
+    fputs(temporal,fout);
+}
+c++;
+}
+    
+}
+//FEATURE 2
+void feature2(FILE *fin, FILE *fout){
+int c=0;
+char temporal[70];
+
+
+fin=fopen("fin","r");
+if(fin==NULL){
+    perror("Error en la apertura del archivo");
+    return (EXIT_FAILURE);
+}
+
+
+if((fout=fopen(fout,"w"))==NULL){
+    
+    exit(1);
+}
+
+ 
+while(!feof(fin)){
+fgets(temporal,70,fin);
+
+if(c==0){
+    fputs(temporal,fout);
+}
+if(c==1){
+int pi,pf;
+char aux;
+
+pi=0;
+pf=strlen(temporal)-1;
+while(pi<pf){
+aux=temporal[pi];
+temporal[pi]=temporal[pf];
+temporal[pf]=aux;
+pi++; 
+pf--;
+}
+fputs(temporal,fout);
+}
+c++;
+}
+  
+
+
+}
