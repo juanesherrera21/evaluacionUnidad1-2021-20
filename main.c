1d2cdcedc1987159cd01b0046390b865f5c3e350
juanesherrera21 <68380237+juanesherrera21@users.noreply.github.com>
2021-08-27 21:50:39 -0500

    feauture 3
    feauture 3 implemantado y correccion, funcionando

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "student.h"
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
 	
 	feature1(argv[1],argv[2]);
 	
 	feature2(argv[1],argv[2]);
-    return EXIT_SUCCESS;
-}
+	
+	feature3(argv[1],argv[2]);
+//feature 5	
+
+
+
+
+return EXIT_SUCCESS;	
+	
+	}
+	
+
--- student.c
+++ student.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include "student.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
 //FEATURE1
 void feature1(FILE *fin, FILE *fout){
 int c=0;
 char temporal[70];
 
 
-fin=fopen("fin","r");
+fin=fopen(fin,"r");
 if(fin==NULL){
     perror("Error en la apertura del archivo");
     return (EXIT_FAILURE);
@@ -35,7 +39,7 @@ int c=0;
 char temporal[70];
 
 
-fin=fopen("fin","r");
+fin=fopen(fin,"r");
 if(fin==NULL){
     perror("Error en la apertura del archivo");
     return (EXIT_FAILURE);
@@ -71,7 +75,68 @@ fputs(temporal,fout);
 }
 c++;
 }
-  
+  }
+  //FEATURE 3
+void feature3(FILE *fin, FILE *fout){
+int c=0;
+char temporal[70];
 
+
+fin=fopen(fin,"r");
+if(fin==NULL){
+    perror("Error en la apertura del archivo");
+    return (EXIT_FAILURE);
+}
+
+
+if((fout=fopen(fout,"w"))==NULL){
+    
+    exit(1);
+}
+
+ 
+while(!feof(fin)){
+fgets(temporal,70,fin);
+if(c==0){
+    fputs(temporal,fout);
+}
+if(c==1){
+int pi,pf;
+char aux;
+
+pi=0;
+pf=strlen(temporal)-1;
+while(pi<pf){
+aux=temporal[pi];
+temporal[pi]=temporal[pf];
+temporal[pf]=aux;
+pi++; 
+pf--;
+}
+fputs(temporal,fout);
+}
+if(c==2){
+fputs(temporal,fout);
+}
+if(c==3){
+char *token;
+int sumatotal;
+sumatotal=0;
+token = strtok(temporal, " ");
+if(token==NULL){
+printf("NO hay token");
+}
+sumatotal += atoi(token);
+while(token!= NULL){
+token=strtok(NULL, " ");
+if(token!=NULL){
+sumatotal+=atoi(token);
+}
+}
+fprintf(fout,"%d\n",sumatotal);
 
 }
+c++;
+}
+}
+
